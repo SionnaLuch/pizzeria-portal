a1b2909bd292b5982dad93430b4f265eade12584
Sionna Poleszuk <sionna.pol@gmail.com>
2022-03-12 21:42:02 +0100

    pageNav

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import { StylesProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core/createMuiTheme';
+import { ThemeProvider } from '@material-ui/styles';
+import Kitchen from './components/views/Kitchen/Kitchen';
+import Login from './components/views/Login/Login';
+import Tables from './components/views/Tables/Tables';
+import Waiter from './components/views/Waiter/Waiter';
+import Dashboard from './components/views/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Now!
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
